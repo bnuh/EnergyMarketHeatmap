151dff3091ac79f3fa67cbcfbb4dda6686998f1d
bryanmelanson <bryanm@mun.ca>
2016-01-19 12:45:13 -0330

    LBMP and additional column added for pricing display

--- mappointsNY2.R
+++ mappointsNY2.R
@@ -1,4 +1,4 @@
-library(dismo)  # check also the nice 'rgbif' package! 
+library(dismo)  # check also the nice 'rgbif' package!
 laurus <- gbif("Laurus", "nobilis")
 
 # get data frame with spatial coordinates (points)
@@ -11,61 +11,77 @@ locs <- subset(locs, locs$lat < 90)
 
 
 coordinates(locs) <- c("lon", "lat")  # set spatial coordinates
-plot(locs)
+##plot(locs)
 
-crs.geo <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
+crs.geo <-
+  CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
 proj4string(locs) <- crs.geo  # define projection system of our data
 summary(locs)
 
-plot(locs, pch = 20, col = "steelblue")
+## plot(locs, pch = 20, col = "steelblue")
 library(rworldmap)
 # library rworldmap provides different types of global maps, e.g:
 data(coastsCoarse)
 data(countriesLow)
-plot(coastsCoarse, add = T)
+##plot(coastsCoarse, add = T)
 
-table(locs$country)  # see localities of Laurus nobilis by country
-
-
-locs.gb <- subset(locs, locs$country == "United Kingdom")  # select only locs in UK
-plot(locs.gb, pch = 20, cex = 2, col = "steelblue")
-title("Laurus nobilis occurrences in UK")
-plot(countriesLow, add = T)
+locs.gb <-
+  subset(locs, locs$country == "United Kingdom")  # select only locs in UK
+## plot(locs.gb, pch = 20, cex = 2, col = "steelblue")
+##plot(countriesLow, add = T)
 
 gbmap <- gmap(locs.gb, type = "satellite")
-locs.gb.merc <- Mercator(locs.gb)  # Google Maps are in Mercator projection. 
+locs.gb.merc <-
+  Mercator(locs.gb)  # Google Maps are in Mercator projection.
 # This function projects the points to that projection to enable mapping
-plot(gbmap)
+##plot(gbmap)
 
 points(locs.gb.merc, pch = 20, col = "red")
 
 require(RgoogleMaps)
 
-locs.gb.coords <- as.data.frame(coordinates(locs.gb))  # retrieves coordinates 
+locs.gb.coords <-
+  as.data.frame(coordinates(locs.gb))  # retrieves coordinates
 # (1st column for longitude, 2nd column for latitude)
-PlotOnStaticMap(lat = locs.gb.coords$lat, lon = locs.gb.coords$lon, zoom = 5, 
-                cex = 1.4, pch = 19, col = "red", FUN = points, add = F)
+PlotOnStaticMap(
+  lat = locs.gb.coords$lat, lon = locs.gb.coords$lon, zoom = 5,
+  cex = 1.4, pch = 19, col = "red", FUN = points, add = F
+)
 
-map.lim <- qbbox(locs.gb.coords$lat, locs.gb.coords$lon, TYPE = "all")  # define region 
+map.lim <-
+  qbbox(locs.gb.coords$lat, locs.gb.coords$lon, TYPE = "all")  # define region
 # of interest (bounding box)
-mymap <- GetMap.bbox(map.lim$lonR, map.lim$latR, destfile = "gmap.png", maptype = "terrain")
+mymap <-
+  GetMap.bbox(map.lim$lonR, map.lim$latR, destfile = "gmap.png", maptype = "terrain")
 
-iconSel=paste0("{","'default': {'normal': 'https://helpdesk.lastpass.com/wp-content/uploads/sites1.png',\n",
-               "'selected': 'http://webking.com.hk/web-form/images/shared_icons/new_menu_icons/14.gif'",
-               "}}")
+iconSel = paste0(
+  "{","'default': {'normal': 'https://helpdesk.lastpass.com/wp-content/uploads/sites1.png',\n",
+  "'selected': 'http://webking.com.hk/web-form/images/shared_icons/new_menu_icons/14.gif'",
+  "}}"
+)
 
 # see the file in the wd
-PlotOnStaticMap(mymap, lat = locs.gb.coords$lat, lon = locs.gb.coords$lon, zoom = NULL, 
-                cex = 1.3, pch = 19, col = "red", FUN = points, add = F)
+PlotOnStaticMap(
+  mymap, lat = locs.gb.coords$lat, lon = locs.gb.coords$lon, zoom = NULL,
+  cex = 1.3, pch = 19, col = "red", FUN = points, add = F
+)
 
-mymap <- GetMap.bbox(map.lim$lonR, map.lim$latR, destfile = "gmap.png", maptype = "hybrid")
+mymap <-
+  GetMap.bbox(map.lim$lonR, map.lim$latR, destfile = "gmap.png", maptype = "hybrid")
 
-PlotOnStaticMap(mymap, lat = locs.gb.coords$lat, lon = locs.gb.coords$lon, zoom = NULL, 
-                cex = 1.3, pch = 19, col = "red", FUN = points, add = F)
+PlotOnStaticMap(
+  mymap, lat = locs.gb.coords$lat, lon = locs.gb.coords$lon, zoom = NULL,
+  cex = 1.3, pch = 19, col = "red", FUN = points, add = F
+)
 
 points.gb <- as.data.frame(locs.gb)
-points.gb$latlon <- paste(points.gb$lat, points.gb$lon, sep=":")
-map.gb <- gvisMap(points.gb, locationvar="latlon", tipvar="lat", 
-                  options = list(showTip=T, icons = iconSel, showLine=F, enableScrollWheel=TRUE,
-                                 useMapTypeControl=T, width=1400,height=800))
+points.gb$price <- paste("LBMP: ", points.gb$lat , sep="")
+points.gb$latlon <- paste(points.gb$lat, points.gb$lon, sep = ":")
+map.gb <- gvisMap(
+  points.gb, locationvar = "latlon", tipvar = "price",
+  options = list(
+    showTip = T, icons = iconSel, showLine = F, enableScrollWheel = TRUE,
+    useMapTypeControl = T, width = 1400,height = 800
+  )
+)
 plot(map.gb)
