5c2a16f791d45c08f1807884b87a45cf73578563
bnuh <bryan@okplus.ca>
2016-01-19 21:24:14 -0330

    Clean up

--- mappointsNY2.R
+++ mappointsNY2.R
@@ -1,14 +1,14 @@
 library(dismo)  # check also the nice 'rgbif' package!
 laurus <- gbif("Laurus", "nobilis")
 
+library(maps)
+test <- map('state', region = 'New York')
+
 # get data frame with spatial coordinates (points)
 locs <- subset(laurus, select = c("country", "lat", "lon"))
-head(locs)  # a simple data frame with coordinates
-
 
 # Discard data with errors in coordinates:
 locs <- subset(locs, locs$lat < 90)
-
 
 coordinates(locs) <- c("lon", "lat")  # set spatial coordinates
 ##plot(locs)
@@ -18,34 +18,19 @@ crs.geo <-
 proj4string(locs) <- crs.geo  # define projection system of our data
 summary(locs)
 
-## plot(locs, pch = 20, col = "steelblue")
 library(rworldmap)
-# library rworldmap provides different types of global maps, e.g:
-data(coastsCoarse)
-data(countriesLow)
 
-locs.gb <-
-  subset(locs, locs$country == "United Kingdom")  # select only locs in UK
+locs.gb <- subset(locs, locs$country == "United Kingdom")  # select only locs in UK
 ## plot(locs.gb, pch = 20, cex = 2, col = "steelblue")
 ##plot(countriesLow, add = T)
 
-gbmap <- gmap(locs.gb, type = "terrain")
-locs.gb.merc <-
-  Mercator(locs.gb)  # Google Maps are in Mercator projection.
+locs.gb.merc <- Mercator(locs.gb)  # Google Maps are in Mercator projection.
 # This function projects the points to that projection to enable mapping
 ##plot(gbmap)
 
 require(RgoogleMaps)
 
-locs.gb.coords <-
-  as.data.frame(coordinates(locs.gb))  # retrieves coordinates
-# (1st column for longitude, 2nd column for latitude)
-
-map.lim <-
-  qbbox(locs.gb.coords$lat, locs.gb.coords$lon, TYPE = "all")  # define region
-# of interest (bounding box)
-mymap <-
-  GetMap.bbox(map.lim$lonR, map.lim$latR, destfile = "gmap.png", maptype = "terrain")
+locs.gb.coords <- as.data.frame(coordinates(locs.gb)) 
 
 iconSel = paste0(
   "{","'default': {'normal': 'https://helpdesk.lastpass.com/wp-content/uploads/sites1.png',\n",
@@ -72,10 +57,6 @@ plot(map.gb)
 htmlOut = print(map.gb)
 write(htmlOut, file = "")
 
-plot(gbmap)
-mypolygon <- drawPoly()  # click on the map to draw a polygon and press ESC when finished
-summary(mypolygon)  # now you have a spatial polygon! Easy, isn't it?
-
 crs.geo <-
   CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
 proj4string(locs) <- crs.geo  # define projection system of our data
@@ -89,15 +70,13 @@ fromDisk(tmin1)
 tmin1 <- tmin1/10  # Worldclim temperature data come in decimal degrees 
 tmin1  # look at the info
 
-plot(tmin1)
+plot(tmin1, axes = F, legend = F, box = F)
 
 library(gtools)
-#file.remove(paste(getwd(), "/wc10/", "tmin_10m_bil.zip", sep = ""))
+file.remove(paste(getwd(), "/wc10/", "tmin_10m_bil.zip", sep = ""))
 
 list.ras <- mixedsort(list.files(paste(getwd(), "/wc10/", sep = ""), full.names = T, 
                                  pattern = ".bil"))
-
-list.ras  # I have just collected a list of the files containing monthly temperature values
 
 tmin.all <- stack(list.ras)
 
@@ -114,62 +93,24 @@ projection(tmin1.c) <- crs.geo
 projection(tmin.all.c) <- crs.geo
 
 library(lattice) 
-require(stats)
 
-plot(tmin1.c)
-
-
-head(locs)  # we'll obtain tmin values for our points
-##   country
-## 1   Spain
-## 2   Spain
-## 3   Spain
-## 4   Spain
-## 5   Spain
-## 6   Spain
 projection(tmin1) <- crs.geo
-locs$tmin1 <- extract(tmin1, locs)  # raster values 
-# are incorporated to the dataframe
-head(locs)
-##   country tmin1
-## 1   Spain   6.7
-## 2   Spain   2.1
-## 3   Spain   6.7
-## 4   Spain   4.2
-## 5   Spain   6.2
-## 6   Spain   6.7
+locs$tmin1 <- extract(tmin1, locs)
 
-plot(tmin1.c)
 reg.clim <- extract(tmin1.c, drawExtent())  # click twice to 
 # draw extent of the region of interest
 summary(reg.clim)
 
 # rasterToPoints
 tminvals <- rasterToPoints(tmin1.c)
-head(tminvals)
-##            x     y tmin1
-## [1,] -6.4167 49.92   4.2
-## [2,] -6.2500 49.92   4.2
-## [3,] -5.2500 49.92   2.4
-## [4,]  0.5833 49.92   1.0
-## [5,]  0.7500 49.92   1.0
-## [6,]  0.9167 49.92   1.0
 
-plot(tmin1.c)
 click(tmin1.c, n = 3)  # click n times in the map to get values
 
 locs2ras <- rasterize(locs.gb, tmin1, field = rep(1, nrow(locs.gb)))
-locs2ras
-## class       : RasterLayer 
-## dimensions  : 900, 2160, 1944000  (nrow, ncol, ncell)
-## resolution  : 0.1667, 0.1667  (x, y)
-## extent      : -180, 180, -60, 90  (xmin, xmax, ymin, ymax)
-## coord. ref. : +proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0 
-## data source : in memory
-## names       : layer 
-## values      : 1, 1  (min, max)
-plot(locs2ras, xlim = c(-5, 5), ylim = c(45, 60), legend = F)
-#data(wrld_simpl)
+
+plot(locs2ras, xlim = c(-5, 5), ylim = c(45, 60), legend = F, axes = F, box = F)
+plot(countriesLow, add = T)
+
 plot(tmin1, add = T)
 
 
@@ -279,3 +220,8 @@ map.gb <- gvisMap(
   )
 )
 plot(map.gb)
+
+#####
+
+library(maps)
+test <- map('state', region = 'New York')
